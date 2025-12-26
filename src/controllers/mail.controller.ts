77a882b5a2e57ce1b413e384fd053da3ab640e49
anujaraired <anuj@adaired.org>
2025-12-26 16:49:09 +0530

    update route of mail

--- src/controllers/mail.controller.ts
+++ src/controllers/mail.controller.ts
@@ -10,8 +10,8 @@ export const sendMail = async (req: Request, res: Response) => {
       port: 465,
       secure: true,
       auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
       },
       // tls: {
       //   rejectUnauthorized: false,
@@ -19,8 +19,9 @@ export const sendMail = async (req: Request, res: Response) => {
     });
 
     await transporter.sendMail({
-      from: `"New Inquiry" <${process.env.MAIL_USER}>`,
-      to: ["dheeraj@adaired.com", "sahil@adaired.com", "ratanjyot@adaired.org"],
+      from: `"New Inquiry" <${process.env.EMAIL_USERNAME}>`,
+      // to: ["dheeraj@adaired.com", "sahil@adaired.com", "anuj@adaired.org"],
+      to: ["anuj@adaired.org"],
 
       subject: `New Inquiry - ${formId}`,
       html: `
--- src/routes/mail.routes.ts
+++ src/routes/mail.routes.ts
@@ -2,7 +2,10 @@ import { Router } from "express";
 import { sendMail } from "../controllers/mail.controller";
 
 const router = Router();
-
+const he = "dsfsf";
+const check = () => {
+  console.log("fsdmfdsf");
+};
 router.post("/send", sendMail);
 
 export default router;
